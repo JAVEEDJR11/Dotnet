fc1e6b1a9b371d1f0724ff25e88a5889ceeb910a
Rin Itsoshi <mmohamedjaveed30@gmail.com>
2024-12-26 10:18:54 +0530

    Add files via upload

---
+++ photo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dotnet_Assignment
+{
+    public class Photo
+    {
+        public int numPages;
+        public void GetNumberPages()
+        {
+            Console.WriteLine(numPages);
+        }
+
+        public Photo()
+        {
+            this.numPages = 16;
+        }
+
+        public Photo(int nPages)
+        {
+            this.numPages = nPages;
+        }
+
+
+        public class BigPhotoBook : Photo
+        {
+            public BigPhotoBook()
+            {
+                this.numPages = 64;
+            }
+        }
+    }
+}
